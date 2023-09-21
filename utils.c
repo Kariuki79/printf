42912bcc26a24d3d8062eb81af5919d592941cbd
maintcode <jmwai2007@gmail.com>
2023-09-21 03:47:31 -0700

    update

--- utils.c
+++ utils.c
@@ -20,6 +20,7 @@ int is_printable(char c)
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
+
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -62,7 +63,7 @@ long int convert_size_number(long int num, int size)
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number 
+ * @num: Number
  * @size: Number indicating the type to be casted
  * Return: value of num
  */
