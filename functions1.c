5dbc073714b68d421f4beddb498538a4d101bd01
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-22 20:10:06 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -109,7 +109,7 @@ int print_hexa_upper(va_list types, char buffer[],
  * @map_to: Array of values
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @flag_ch: Calculates active flags
+ * @flag_ch: Calculates flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
@@ -119,7 +119,7 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
-	int i = BUFF_SIZE - 2;
+	int a = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
@@ -128,23 +128,23 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-		buffer[i--] = '0';
+		buffer[a--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
 	while (num > 0)
 	{
-		buffer[i--] = map_to[num % 16];
+		buffer[a--] = map_to[num % 16];
 		num /= 16;
 	}
 
 	if (flags & F_HASH && init_num != 0)
 	{
-		buffer[i--] = flag_ch;
-		buffer[i--] = '0';
+		buffer[a--] = flag_ch;
+		buffer[a--] = '0';
 	}
 
-	i++;
+	a++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+	return (write_unsgnd(0, a, buffer, flags, width, precision, size));
 }
