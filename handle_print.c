7e06e5ece0dc6bbc6e5f7681131ae24a2fc4da5e
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-22 22:28:37 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: string in which to print the arguments
+ * @formatted_string: string in which to print the arguments
  * @list: list of arguments to be printed
  * @index: index
  * @buffer: Buffer array to handle print.
@@ -11,38 +11,38 @@
  * @size: Size specifier
  * Return: (1) or (2)
  */
-int handle_print(const char *fmt, int *index, va_list list, char buffer[],
+int handle_print(const char *formatted_string, int *index, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, len = 0, chars_printed = -1;
-	fmt_t fmt_types[] = {
+	formatted_string_t formatted_string_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*index] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for (i = 0; formatted_string_types[i].formatted_string != '\0'; i++)
+		if (formatted_string[*index] == formatted_string_types[i].formatted_string)
+			return (formatted_string_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (formatted_string_types[i].formatted_string == '\0')
 	{
-		if (fmt[*index] == '\0')
+		if (formatted_string[*index] == '\0')
 			return (-1);
 		len += write(1, "%%", 1);
-		if (fmt[*index - 1] == ' ')
+		if (formatted_string[*index - 1] == ' ')
 			len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*index);
-			while (fmt[*index] != ' ' && fmt[*index] != '%')
+			while (formatted_string[*index] != ' ' && formatted_string[*index] != '%')
 				--(*index);
-			if (fmt[*index] == ' ')
+			if (formatted_string[*index] == ' ')
 				--(*index);
 			return (1);
 		}
-		len += write(1, &fmt[*index], 1);
+		len += write(1, &formatted_string[*index], 1);
 		return (len);
 	}
 	return (chars_printed);
