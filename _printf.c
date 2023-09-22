70e933f7cfd13edb8d293212d2b306cef09ef380
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-22 16:08:23 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,23 +5,16 @@
  * @format: format
  * Return: Printed chars
  */
+void print_buffer(char buffer[], int *buff_ind);
+
 int _printf(const char *format, ...)
 {
     if (format == NULL)
         return (-1);
 
     va_list args;
-    int printed_chars = 0;
-
     va_start(args, format);
-    printed_chars = parse_and_print(format, args);
-    va_end(args);
 
-    return (printed_chars);
-}
-
-int parse_and_print(const char *format, va_list args)
-{
     int printed_chars = 0;
     int buff_ind = 0;
     char buffer[BUFF_SIZE];
@@ -31,39 +24,52 @@ int parse_and_print(const char *format, va_list args)
         if (format[i] != '%')
         {
             buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                printed_chars += print_buffer(buffer, &buff_ind);
+            if (buff_ind == BUFF_SIZE || format[i + 1] == '\0')
+            {
+                write(1, buffer, buff_ind);
+                printed_chars += buff_ind;
+                buff_ind = 0;
+            }
+            else
+            {
+                printed_chars++;
+            }
         }
         else
         {
             i++;
             if (format[i] == '\0')
+            {
+                va_end(args);
                 return (-1);
+            }
 
             int flags = get_flags(format, &i);
             int width = get_width(format, &i, args);
             int precision = get_precision(format, &i, args);
             int size = get_size(format, &i);
-
             int printed = handle_print(format[i], args, buffer, flags, width, precision, size);
+
             if (printed == -1)
+            {
+                va_end(args);
                 return (-1);
+            }
+
             printed_chars += printed;
         }
     }
 
-    printed_chars += print_buffer(buffer, &buff_ind);
+    va_end(args);
 
-    return printed_chars;
+    return (printed_chars);
 }
 
-int print_buffer(char buffer[], int *buff_ind)
+void print_buffer(char buffer[], int *buff_ind)
 {
     if (*buff_ind > 0)
     {
         write(1, buffer, *buff_ind);
         *buff_ind = 0;
-        return *buff_ind;
     }
-    return 0;
 }
