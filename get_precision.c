986c1c56b623794c0f9f5d8c697363243202edea
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-22 21:19:34 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -9,24 +9,24 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int precision = -1;
+	int p = -1;
 
 	if (format[curr_i] != '.')
-		return (precision);
+		return (p);
 
-	precision = 0;
+	p = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
+			p *= 10;
+			p += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			precision = va_arg(list, int);
+			p = va_arg(list, int);
 			break;
 		}
 		else
@@ -35,5 +35,5 @@ int get_precision(const char *format, int *i, va_list list)
 
 	*i = curr_i - 1;
 
-	return (precision);
+	return (p);
 }
