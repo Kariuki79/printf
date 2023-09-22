cddd402bee605466a1f07acc783f78c3c1fa8737
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-22 21:34:38 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -3,7 +3,7 @@
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments
  * @list: list of arguments to be printed
- * @ind: index
+ * @index: index
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
  * @width: width
@@ -11,10 +11,10 @@
  * @size: Size specifier
  * Return: (1) or (2)
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int handle_print(const char *fmt, int *index, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int i, len = 0, chars_printed = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -23,27 +23,27 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
+		if (fmt[*index] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
-		if (fmt[*ind] == '\0')
+		if (fmt[*index] == '\0')
 			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
+		len += write(1, "%%", 1);
+		if (fmt[*index - 1] == ' ')
+			len += write(1, " ", 1);
 		else if (width)
 		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
+			--(*index);
+			while (fmt[*index] != ' ' && fmt[*index] != '%')
+				--(*index);
+			if (fmt[*index] == ' ')
+				--(*index);
 			return (1);
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		len += write(1, &fmt[*index], 1);
+		return (len);
 	}
-	return (printed_chars);
+	return (chars_printed);
 }
