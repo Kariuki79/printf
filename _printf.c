54fbd3673669035b42167ef19bd8c7be82db7fa9
Kariuki Mwangi <139071098+Kariuki79@users.noreply.github.com>
2023-09-22 17:06:49 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format
  * Return: Printed chars
  */
+
 int _printf(const char *format, ...)
 {
     va_list list;
@@ -17,6 +18,7 @@ int _printf(const char *format, ...)
     
     if (format == NULL)
         return (-1);
+
     for (i = 0; format[i]; i++)
     {
         if (format[i] != '%')
@@ -35,21 +37,35 @@ int _printf(const char *format, ...)
         else
         {
             if (format[i + 1] == '\0')
+            {
+                va_end(list);
                 return (-1);
+            }
 
-            buff_ind += handle_print(format[i + 1], list, buffer + buff_ind);
-            if (buff_ind == -1)
+            int specifier_length = handle_print(format[i + 1], list, buffer, &buff_ind);
+            if (specifier_length == -1)
             {
                 va_end(list);
                 return (-1);
             }
 
             i++; // Skip the format specifier
-            printed_chars += buff_ind;
-            buff_ind = 0;
+            printed_chars += specifier_length;
         }
     }
 
     va_end(list);
-    return printed_chars;
+    return (printed_chars);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
